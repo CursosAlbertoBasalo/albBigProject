9ebb8524afaa372193b3fcbd89450915c66e17b7
Alberto Basalo <albertobasalo@hotmail.com>
2022-05-04 10:14:16 +0200

    feat: multiprojects

---
+++ projects/ui/src/lib/ui.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'alb-ui-ui',
+  template: ` <h2>ui works!</h2> `,
+  styles: [],
+})
+export class UiComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
